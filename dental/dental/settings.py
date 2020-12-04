208090b951c60ce132582abbc7f5f15faee22f7e
MMR <32247752+MMR-1045@users.noreply.github.com>
2020-12-04 20:17:57 +0600

    Update settings.py

--- dental/dental/settings.py
+++ dental/dental/settings.py
@@ -10,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%qad#v0=8(x^&f5xdu1q=1#+r#p3obp+td&-*m49q=^-&&9%j1'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
